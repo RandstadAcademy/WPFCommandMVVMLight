572dc78787789ac5786a054d338f6e98ca7d22a7
Tommaso <randstadacademydotnet@gmail.com>
2017-02-15 11:42:03 +0100

    Add project files.

---
+++ MyCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+  public class MyCommand : ICommand
+  {
+    private Action _executeCode;
+    private Func<bool> _canExecuteCode;
+
+    public event EventHandler CanExecuteChanged;
+
+    public MyCommand(Action executeCode)
+    {
+      _executeCode = executeCode;
+    }
+    public MyCommand(Action executeCode, Func<bool> canExecuteCode)
+    {
+      _executeCode = executeCode;
+      _canExecuteCode = canExecuteCode;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+      if (_canExecuteCode != null)
+        return _canExecuteCode();
+
+      return true;
+    }
+
+    public void Execute(object parameter)
+    {
+      if (_executeCode != null)
+        _executeCode();
+    }
+  }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1
+{
+  public class MainViewModel
+  {
+    public MyCommand TestCommand { get; set; }
+    public MyCommand EnableCommand { get; set; }
+
+    private bool testEnabled = false;
+
+    public MainViewModel()
+    {
+      TestCommand = new MyCommand(TestCommanExecute, () => { return TestCommandCanExecute(); });
+      EnableCommand = new MyCommand(EnableCommandExecute);
+    }
+
+  protected void TestCommanExecute()
+    {
+      // DO something
+
+    }
+
+    protected bool TestCommandCanExecute()
+    {
+      return testEnabled;
+    }
+
+    protected void EnableCommandExecute()
+    {
+      testEnabled = true;
+      
+    }
+
+  }
+}
